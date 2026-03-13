3447e75f17144ad2ca27f0ef186e66beb9e556c6
Nick Murray <nick@mountainairweb.com>
2026-03-13 13:05:53 -0700

    partner tax's

--- inc/custom-taxonomy.php
+++ inc/custom-taxonomy.php
@@ -71,5 +71,65 @@ function gdt_partner_type_reg() {
   register_taxonomy( 'partner_type', array( 'partner' ), $args );
 }
 
+// register Focus Areas custom taxonomy
+add_action( 'init', 'gdt_focus_areas_reg', 0 );
+
+function gdt_focus_areas_reg() {
+  $singular = 'Focus Area';
+  $plural = 'Focus Areas'; 
+  $labels = array(
+    'name'              => "$plural",
+    'singular_name'     => "$singular",
+    'search_items'      => "Search $plural",
+    'all_items'         => "All $plural",
+    'edit_item'         => "Edit $singular",
+    'update_item'       => "Update $singular",
+    'add_new_item'      => "Add New $singular",
+    'new_item_name'     => "New $singular Name",
+    'menu_name'         => "$plural"
+  );
+  $args = array(
+    'public'            => true,
+    'show_in_rest'      => true,
+    'hierarchical'      => false,
+    'labels'            => $labels,
+    'show_ui'           => true,
+    'show_admin_column' => true,
+    'query_var'         => true,
+    'rewrite'           => array( 'slug' => 'focus-area', 'with_front' => false )
+  );
+  register_taxonomy( 'focus_area', array( 'partner' ), $args );
+}
+
+// register Specialty custom taxonomy
+add_action( 'init', 'gdt_specialty_reg', 0 ); 
+
+function gdt_specialty_reg() {
+  $singular = 'Specialty';
+  $plural = 'Specialties';
+  $labels = array(
+    'name'              => "$plural",
+    'singular_name'     => "$singular",
+    'search_items'      => "Search $plural",
+    'all_items'         => "All $plural",
+    'edit_item'         => "Edit $singular",
+    'update_item'       => "Update $singular",
+    'add_new_item'      => "Add New $singular",
+    'new_item_name'     => "New $singular Name",
+    'menu_name'         => "$plural"
+  );
+  $args = array(
+    'public'            => true,
+    'show_in_rest'      => true,
+    'hierarchical'      => false,
+    'labels'            => $labels,
+    'show_ui'           => true,
+    'show_admin_column' => true,
+    'query_var'         => true,
+    'rewrite'           => array( 'slug' => 'specialty', 'with_front' => false )
+  );
+  register_taxonomy( 'specialty', array( 'partner' ), $args );
+}
+
 
 ?>
